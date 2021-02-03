1e941245a828d778fe7d37a73212b61fcf270ee2
CompuWiser <CompuWiser@outlook.com>
2021-02-03 22:11:54 +0200

    complted initializaions

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6e9fed85b5054eba8ff82b5882a481e4'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f0f6f1ee-cdcb-4d6e-84c6-2c34b146e214'
+};
